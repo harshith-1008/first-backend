dcc0576ec77e790c2c4195c5485a5f56ae9a20d8
harshith-1008 <saiharshith.poli@gmail.com>
2024-05-09 19:35:07 +0530

    user and video model with boiler code for jwt and bcrypt

--- src/index.js
+++ src/index.js
@@ -1,6 +1,19 @@
 import dotenv from "dotenv";
 import connectDB from "./db/index.js";
+import { app } from "./apps.js";
 dotenv.config({
   path: "./env",
 });
-connectDB();
+connectDB()
+  .then(() => {
+    app.on((error) => {
+      console.log("error occurred in app ", error);
+    });
+
+    app.listen(process.env.PORT || 8000, () => {
+      console.log(`process is running on port ${process.env.PORT}`);
+    });
+  })
+  .catch((error) => {
+    console.log("mongo db connection failed");
+  });
